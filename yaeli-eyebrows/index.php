183bf3f372eafef45ed966a96e356343e5231475
Hai Atoon <59227933+HaiAtoon@users.noreply.github.com>
2023-09-10 17:20:24 +0300

    Update index.php

--- yaeli-eyebrows/index.php
+++ yaeli-eyebrows/index.php
@@ -60,7 +60,7 @@
   *
   */
 
- require_once __DIR__ . '/vendor/autoload.php';
+ require_once __DIR__ . '/application/config/autoload.php';
 
 /*
  *---------------------------------------------------------------
